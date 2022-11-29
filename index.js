ba93b0b065cc81e704e74bbeb908f9153492e59b
dtpvy <dtpvy0202@gmail.com>
2022-11-29 08:37:06 +0700

    change file html to hbs

--- index.js
+++ index.js
@@ -15,7 +15,19 @@ app.engine(
 );
 app.set('view engine', 'hbs');
 
-app.get('/', (req, res) => {
+app.get('/task1', (req, res) => {
+  res.render('task1');
+});
+app.get('/task2', (req, res) => {
+  res.render('task2');
+});
+app.get('/task3', (req, res) => {
+  res.render('task3');
+});
+app.get('/task4', (req, res) => {
+  res.render('task4');
+});
+app.get('/*', (req, res) => {
   res.render('index');
 });
 
